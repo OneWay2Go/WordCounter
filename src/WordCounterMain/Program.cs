4f511306a535150982d497e396aa9ebf9724fe2a
OneWay2Go <uchqunov.m@yandex.com>
2025-07-16 01:14:51 +0500

    WordCounter

---
+++ src/WordCounterMain/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace WordCounterMain;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Paste your text here to be counted(type f to finish pasting the text): ");
+
+        string? line;
+        var lines = new List<string>();
+
+        while((line = Console.ReadLine()) != null)
+        {
+            if (line.Trim() == "f")
+            {
+                break;
+            }
+
+            lines.Add(line);
+        }
+
+        var input = string.Join(Environment.NewLine, lines);
+
+        var wordCounter = new WordCounter();
+
+        var result = wordCounter.CountWords(input);
+
+        Console.WriteLine(result);
+    }
+}
+
+public class WordCounter
+{
+    public int CountWords(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return 0;
+        }
+
+        var splittedText = text.Split(new[] {' ', '\r', '\n', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+
+        return splittedText.Length;
+    }
+}
